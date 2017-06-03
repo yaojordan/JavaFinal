d3dedac6096d902ec638aee28fe8b4b99aad34f3
宇鴻 <yaojordan@gmail.com>
2017-06-03 15:21:35 +0800

    0603

--- main.java
+++ main.java
@@ -84,7 +84,7 @@ public class main extends JFrame implements ActionListener {
 	static Label DiscreteLab = new Label("離散數學");
 	static Label ElectricLab = new Label("電子電路學");
 	static Label ElectriclabLab = new Label("電子電路學實驗");
-	static Label SystemAssemblyLab = new Label("系統程式與組合語言");
+	static Label SystemAssemblyLab = new Label("系統程式組合語言");
 	static Label ProbabilityLab = new Label("機率統計");
 	static Label LinearAlgebraLab = new Label("線性代數");
 	static Label ProgrammingLanguageLab = new Label("程式語言");
@@ -127,7 +127,7 @@ public class main extends JFrame implements ActionListener {
 	static TextField FinalProgram1TextField = new TextField("60");
 	static TextField CompilerTextField = new TextField("60");
 	
-	static Label OOPDLab = new Label("物件導向分析與設計");
+	static Label OOPDLab = new Label("物件導向分析設計");
 	static Label SMLab = new Label("軟體專案管理");//MEDI
 	static Label WebPLab = new Label("WEB程式設計");//ELEC,MEDI
 	static Label UXLab = new Label("使用者經驗");//MEDI
@@ -149,7 +149,7 @@ public class main extends JFrame implements ActionListener {
 	static Label ALogicLab = new Label("進階邏輯設計");
 	static Label WifiLab = new Label("無線感測網路");//MEDI
 	static Label VLSILab = new Label("VLSI設計導論");
-	static Label EmbLab = new Label("嵌入式系統設計與實作");
+	static Label EmbLab = new Label("嵌入式系統實作");
 	static Label SignalLab = new Label("數位訊號處理導論");
 	static TextField ALogicTextField = new TextField("0");
 	static TextField WifiTextField = new TextField("0");
@@ -175,7 +175,7 @@ public class main extends JFrame implements ActionListener {
 	static Label DigitalMaterialLab = new Label("數位創意材料");
 	static Label DigitalMediLab = new Label("數位多媒體");
 	static Label EmbeddedSystemLab = new Label("嵌入式系統導論");//必修必選(ELEC)
-	static Label ProtableDeviceLab = new Label("行動裝置系統與應用");//ELEC
+	static Label ProtableDeviceLab = new Label("行動裝置系統應用");//ELEC
 	static TextField DigitalCreativeTextField = new TextField("60");
 	static TextField CreativeTendTextField = new TextField("60");
 	static TextField InternetCreativeTextField = new TextField("60");
@@ -187,7 +187,7 @@ public class main extends JFrame implements ActionListener {
 	static TextField ProtableDeviceTextField = new TextField("60");
 	
 	//必修必選(ELEC)
-	static Label HardwareLab = new Label("硬體描述語言設計與模擬");
+	static Label HardwareLab = new Label("硬體描述語言模擬");
 	static TextField HardwareTextField = new TextField("60");
 	static Label MicroProcessorLab = new Label("微算機系統");
 	static TextField MicroProcessorTextField = new TextField("60");
@@ -245,7 +245,7 @@ public class main extends JFrame implements ActionListener {
 		p1.setVisible(false);
 		p2.setLayout(null);
 		
-		lab1.setBounds(230, 30, 50, 20);
+		lab1.setBounds(260, 30, 50, 20);
 		ChineseLab1.setBounds(210, 80, 50, 20);
 		ChineseTextField1.setBounds(270,80,30,20);
 		ChineseLab2.setBounds(210, 110, 50, 20);
@@ -296,23 +296,23 @@ public class main extends JFrame implements ActionListener {
 		p2.setVisible(false);
 		p3.setLayout(null);
 		
-		lab1.setBounds(230, 30, 50, 20);
-		selectBox1.setBounds(210, 80, 100, 20);
+		lab1.setBounds(260, 30, 50, 20);
+		selectBox1.setBounds(210, 80, 108, 20);
 		GeneralCourse1.setBounds(320, 80, 30, 20);
-		selectBox2.setBounds(210, 110, 100, 20);
+		selectBox2.setBounds(210, 110, 108, 20);
 		GeneralCourse2.setBounds(320, 110, 30, 20);
-		selectBox3.setBounds(210, 140, 100, 20);
+		selectBox3.setBounds(210, 140, 108, 20);
 		GeneralCourse3.setBounds(320, 140, 30, 20);
-		selectBox4.setBounds(210, 170, 100, 20);
+		selectBox4.setBounds(210, 170, 108, 20);
 		GeneralCourse4.setBounds(320, 170, 30, 20);
-		selectBox5.setBounds(210, 200, 100, 20);
+		selectBox5.setBounds(210, 200, 108, 20);
 		GeneralCourse5.setBounds(320, 200, 30, 20);
-		selectBox6.setBounds(210, 230, 100, 20);
+		selectBox6.setBounds(210, 230, 108, 20);
 		GeneralCourse6.setBounds(320, 230, 30, 20);
-		selectBox7.setBounds(210, 260, 100, 20);
+		selectBox7.setBounds(210, 260, 108, 20);
 		GeneralCourse7.setBounds(320, 260, 30, 20);
 		nextBtn3.setBounds(475, 400, 80, 30);
-		String str[]={"請點選領域", "人文學科", "社會學科", "自然學科", "文明與經典領域學科"} ;  
+		String str[]={"請點選領域", "人文學科", "社會學科", "自然學科", "文明經典領域學科"} ;  
 		for(int i=0; i < str.length; i++)
 		{
 			selectBox1.addItem(str[i]);
@@ -349,10 +349,25 @@ public class main extends JFrame implements ActionListener {
 		stepCount = 3;
 		p4.setVisible(true);
 		p3.setVisible(false);
-		
 		Label title = new Label("軟工組");
-		p4.add(title);
+		p4.setLayout(null);
+		title.setBounds(260, 30, 50, 20);
 		
+		CalculusLab1.setBounds(20, 80, 100, 20);
+		CalculusTextField1.setBounds(120, 80, 30, 20);
+		CalculusLab2.setBounds(20, 110, 100, 20);
+		CalculusTextField2.setBounds(120, 110, 30, 20);
+		PhysicsLab.setBounds(20, 140, 100, 20);
+		PhysicsTextField.setBounds(120, 140, 30, 20);
+		BCCLab.setBounds(20, 170, 100, 20);
+		BCCTextField.setBounds(120, 170, 30, 20);
+		CProgrammingLab.setBounds(20, 200, 100, 20);
+		CProgrammingTextField.setBounds(120, 200, 30, 20);
+		SoftwareLab.setBounds(20, 230, 100, 20);
+		SoftwareTextField.setBounds(120, 230, 30, 20);
+		JavaLab.setBounds(20, 260, 100, 20);
+		JavaTextField.setBounds(120, 260, 30, 20);
+		p4.add(title);
 		p4.add(CalculusLab1);
 		p4.add(CalculusTextField1);
 		p4.add(CalculusLab2);
@@ -363,9 +378,33 @@ public class main extends JFrame implements ActionListener {
 		p4.add(BCCTextField);
 		p4.add(CProgrammingLab);
 		p4.add(CProgrammingTextField);
-		
 		p4.add(SoftwareLab);
 		p4.add(SoftwareTextField);
+		
+		DataStructureLab.setBounds(200, 80, 100, 20);
+		DataStructureTextField.setBounds(300, 80, 30, 20);
+		DiscreteLab.setBounds(200, 110, 100, 20);
+		DiscreteTextField.setBounds(300, 110, 30, 20);
+		ElectricLab.setBounds(200, 140, 100, 20);
+		ElectricTextField.setBounds(300, 140, 30, 20);
+		ElectriclabLab.setBounds(200, 170, 100, 20);
+		ElectriclabTextField.setBounds(300, 170, 30, 20);
+		SystemAssemblyLab.setBounds(200, 200, 100, 20);
+		SystemAssemblyTextField.setBounds(300, 200, 30, 20);
+		ProbabilityLab.setBounds(200, 230, 100, 20);
+		ProbabilityTextField.setBounds(300, 230, 30, 20);
+		LinearAlgebraLab.setBounds(200, 260, 100, 20);
+		LinearAlgebraTextField.setBounds(300, 260, 30, 20);
+		ProgrammingLanguageLab.setBounds(200, 290, 100, 20);
+		ProgrammingLanguageTextField.setBounds(300, 290, 30, 20);
+		TechEngLab.setBounds(200, 320, 100, 20);
+		TechEngTextField.setBounds(300, 320, 30, 20);
+		OOPLab.setBounds(200, 350, 100, 20);
+		OOPTextField.setBounds(300, 350, 30, 20);
+		LogicLab.setBounds(200, 380, 100, 20);
+		LogicTextField.setBounds(300, 380, 30, 20);
+		LogiclabLab.setBounds(200, 410, 100, 20);
+		LogiclabTextField.setBounds(300, 410, 30, 20);
 		p4.add(DataStructureLab);
 		p4.add(DataStructureTextField);
 		p4.add(DiscreteLab);
@@ -382,6 +421,25 @@ public class main extends JFrame implements ActionListener {
 		p4.add(LinearAlgebraTextField);
 		p4.add(ProgrammingLanguageLab);
 		p4.add(ProgrammingLanguageTextField);
+		
+		ComputerOrganLab.setBounds(380, 80, 100, 20);
+		ComputerOrganTextField.setBounds(480, 80, 30, 20);
+		ComputerInternetLab.setBounds(380, 110, 100, 20);
+		ComputerInternetTextField.setBounds(480, 110, 30, 20);
+		AlgorithmLab.setBounds(380, 140, 100, 20);
+		AlgorithmTextField.setBounds(480, 140, 30, 20);
+		OSLab.setBounds(380, 170, 100, 20);
+		OSTextField.setBounds(480, 170, 30, 20);
+		DatabaseLab.setBounds(380, 200, 100, 20);
+		DatabaseTextField.setBounds(480, 200, 30, 20);
+		SoftTestLab.setBounds(380, 230, 100, 20);
+		SoftTestTextField.setBounds(480, 230, 30, 20);
+		CompilerLab.setBounds(380, 260, 100, 20);
+		CompilerTextField.setBounds(480, 260, 30, 20);
+		FinalProgram1Lab.setBounds(380, 290, 100, 20);
+		FinalProgram1TextField.setBounds(480, 290, 30, 20);
+		FinalProgramLab.setBounds(380, 320, 100, 20);
+		FinalProgramTextField.setBounds(480, 320, 30, 20);
 		p4.add(ComputerOrganLab);
 		p4.add(ComputerOrganTextField);
 		p4.add(ComputerInternetLab);
@@ -390,6 +448,7 @@ public class main extends JFrame implements ActionListener {
 		p4.add(AlgorithmTextField);
 		p4.add(OSLab);
 		p4.add(OSTextField);
+		
 		
 		p4.add(JavaLab);
 		p4.add(JavaTextField);
@@ -412,7 +471,7 @@ public class main extends JFrame implements ActionListener {
 		p4.add(FinalProgramLab);
 		p4.add(FinalProgramTextField);
 		
-
+		nextBtn4.setBounds(475, 400, 80, 30);
 		p4.add(nextBtn4);
 		
 		nextBtn4.addActionListener(frame_page1);
@@ -424,10 +483,27 @@ public class main extends JFrame implements ActionListener {
 		stepCount = 3;
 		p4.setVisible(true);
 		p3.setVisible(false);
-		
 		Label title = new Label("資電組");
+		p4.setLayout(null);
+		title.setBounds(260, 30, 50, 20);
 		p4.add(title);
 		
+		CalculusLab1.setBounds(20, 80, 100, 20);
+		CalculusTextField1.setBounds(120, 80, 30, 20);
+		CalculusLab2.setBounds(20, 110, 100, 20);
+		CalculusTextField2.setBounds(120, 110, 30, 20);
+		PhysicsLab.setBounds(20, 140, 100, 20);
+		PhysicsTextField.setBounds(120, 140, 30, 20);
+		BCCLab.setBounds(20, 170, 100, 20);
+		BCCTextField.setBounds(120, 170, 30, 20);
+		CProgrammingLab.setBounds(20, 200, 100, 20);
+		CProgrammingTextField.setBounds(120, 200, 30, 20);
+		ElectricLab.setBounds(20, 230, 100, 20);
+		ElectricTextField.setBounds(120, 230, 30, 20);
+		ElectriclabLab.setBounds(20, 260, 100, 20);
+		ElectriclabTextField.setBounds(120, 260, 30, 20);
+		JavaLab.setBounds(20, 290, 100, 20);
+		JavaTextField.setBounds(120, 290, 30, 20);
 		p4.add(CalculusLab1);
 		p4.add(CalculusTextField1);
 		p4.add(CalculusLab2);
@@ -438,11 +514,33 @@ public class main extends JFrame implements ActionListener {
 		p4.add(BCCTextField);
 		p4.add(CProgrammingLab);
 		p4.add(CProgrammingTextField);
-		
 		p4.add(ElectricLab);
 		p4.add(ElectricTextField);
 		p4.add(ElectriclabLab);
 		p4.add(ElectriclabTextField);
+		
+		DataStructureLab.setBounds(200, 80, 100, 20);
+		DataStructureTextField.setBounds(300, 80, 30, 20);
+		DiscreteLab.setBounds(200, 110, 100, 20);
+		DiscreteTextField.setBounds(300, 110, 30, 20);
+		LogicLab.setBounds(200, 140, 100, 20);
+		LogicTextField.setBounds(300, 140, 30, 20);
+		LogiclabLab.setBounds(200, 170, 100, 20);
+		LogiclabTextField.setBounds(300, 170, 30, 20);
+		EmbeddedSystemLab.setBounds(200, 200, 100, 20);
+		EmbeddedSystemTextField.setBounds(300, 200, 30, 20);
+		SystemAssemblyLab.setBounds(200, 230, 100, 20);
+		SystemAssemblyTextField.setBounds(300, 230, 30, 20);
+		ProbabilityLab.setBounds(200, 260, 100, 20);
+		ProbabilityTextField.setBounds(300, 260, 30, 20);
+		LinearAlgebraLab.setBounds(200, 290, 100, 20);
+		LinearAlgebraTextField.setBounds(300, 290, 30, 20);
+		HardwareLab.setBounds(200, 320, 100, 20);
+		HardwareTextField.setBounds(300, 320, 30, 20);
+		ProgrammingLanguageLab.setBounds(200, 350, 100, 20);
+		ProgrammingLanguageTextField.setBounds(300, 350, 30, 20);
+		TechEngLab.setBounds(200, 380, 100, 20);
+		TechEngTextField.setBounds(300, 380, 30, 20);
 		p4.add(DataStructureLab);
 		p4.add(DataStructureTextField);
 		p4.add(DiscreteLab);
@@ -501,6 +599,8 @@ public class main extends JFrame implements ActionListener {
 		p4.setVisible(true);
 		p3.setVisible(false);
 		Label title = new Label("數創組");
+		p4.setLayout(null);
+		title.setBounds(260, 30, 50, 20);
 		p4.add(title);
 		
 		p4.add(CalculusLab1);
@@ -564,10 +664,38 @@ public class main extends JFrame implements ActionListener {
 	//所有選修課程
 	public static void finalStep(String[] args) {
 		stepCount = 4;
-		
 		p5.setVisible(true);
 		p4.setVisible(false);
 		
+		Label title = new Label("選修課程");
+		p5.setLayout(null);
+		title.setBounds(260, 30, 50, 20);
+		
+		OOPDLab.setBounds(20, 80, 100, 20);
+		OOPDTextField.setBounds(120, 80, 30, 20);
+		SMLab.setBounds(20, 110, 100, 20);
+		SMLTextField.setBounds(120, 110, 30, 20);
+		WebPLab.setBounds(20, 140, 100, 20);
+		WebPTextField.setBounds(120, 140, 30, 20);
+		UXLab.setBounds(20, 170, 100, 20);
+		UXTextField.setBounds(120, 170, 30, 20);
+		ADSLab.setBounds(20, 200, 100, 20);
+		ADSTextField.setBounds(120, 200, 30, 20);
+		SLCLab.setBounds(20, 230, 100, 20);
+		SLCTextField.setBounds(120, 230, 30, 20);
+		UILab.setBounds(20, 260, 100, 20);
+		UITextField.setBounds(120, 260, 30, 20);
+		SSLab.setBounds(20, 290, 100, 20);
+		SSTextField.setBounds(120, 290, 30, 20);
+		CCLab.setBounds(20, 320, 100, 20);
+		CCTextField.setBounds(120, 320, 30, 20);
+		//ProtableDeviceLab.setBounds(20, 350, 100, 20);
+		//ProtableDeviceTextField.setBounds(120, 350, 30, 20);
+		ALogicLab.setBounds(20, 350, 100, 20);
+		ALogicTextField.setBounds(120, 350, 30, 20);
+		WifiLab.setBounds(20, 380, 100, 20);
+		WifiTextField.setBounds(120, 380, 30, 20);
+		p5.add(title);
 		p5.add(OOPDLab);
 		p5.add(OOPDTextField);
 		p5.add(SMLab);
@@ -586,20 +714,31 @@ public class main extends JFrame implements ActionListener {
 		p5.add(SSTextField);
 		p5.add(CCLab);
 		p5.add(CCTextField);
-		
-		p5.add(ProtableDeviceLab);
-		p5.add(ProtableDeviceTextField);
+		//p5.add(ProtableDeviceLab);
+		//p5.add(ProtableDeviceTextField);
 		p5.add(ALogicLab);
 		p5.add(ALogicTextField);
 		p5.add(WifiLab);
 		p5.add(WifiTextField);
+		
+		VLSILab.setBounds(200, 80, 100, 20);
+		VLSITextField.setBounds(300, 80, 30, 20);
+		EmbLab.setBounds(200, 110, 100, 20);
+		EmbTextField.setBounds(300, 110, 30, 20);
+		SignalLab.setBounds(200, 140, 100, 20);
+		SignalTextField.setBounds(300, 140, 30, 20);
+		CreativeLab.setBounds(200, 170, 100, 20);
+		CreativeTextField.setBounds(300, 170, 30, 20);
+		SocialNetLab.setBounds(200, 200, 100, 20);
+		SocialNetTextField.setBounds(300, 200, 30, 20);
+		VideoLab.setBounds(200, 230, 100, 20);
+		VideoTextField.setBounds(300, 230, 30, 20);
 		p5.add(VLSILab);
 		p5.add(VLSITextField);
 		p5.add(EmbLab);
 		p5.add(EmbTextField);
 		p5.add(SignalLab);
 		p5.add(SignalTextField);
-		
 		p5.add(CreativeLab);
 		p5.add(CreativeTextField);
 		p5.add(SocialNetLab);
@@ -607,6 +746,7 @@ public class main extends JFrame implements ActionListener {
 		p5.add(VideoLab);
 		p5.add(VideoTextField);
 		
+		nextBtn5.setBounds(475, 400, 80, 30);
 		p5.add(nextBtn5);
 		nextBtn5.addActionListener(frame_page1);
 		frame_page1.add(p5);
